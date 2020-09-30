46811bfec5b8a05a33720090f5f0292af3c4c2eb
pramith <wmpkw96@gmail.com>
2020-09-30 22:26:38 +0530

    basic file structure

---
+++ android/app/src/main/kotlin/com/example/quickshopping/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quickshopping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Address/adAddress.dart
@@ -0,0 +1,22 @@
+import 'package:quickshopping/Config/config.dart';
+import 'package:quickshopping/Widgets/customAppbar.dart';
+import 'package:quickshopping/Models/address.dart';
+import 'package:flutter/material.dart';
+
+class AddAddress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+    
+    );
+  }
+}
